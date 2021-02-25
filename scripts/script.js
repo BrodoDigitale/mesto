b62072d9b48696030e1371c2ec7289432386c8b4
Julia Sensi <sensi.julia@gmail.com>
2021-02-25 15:36:49 +0100

    first review comments fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,33 @@
 let profile = document.querySelector('.profile');
 let edit = profile.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.edit-form');
+let nameInput = formElement.querySelector('.edit-form__name');
+let jobInput = formElement.querySelector('.edit-form__about');
+let closeButton = popup.querySelector('.popup__close-button');
+let profileName = profile.querySelector('.profile__name');
+let profileAbout = profile.querySelector('.profile__about');
 
+//Функция открытия поп-апа
 function editProfile() {
     popup.classList.add('popup_opened')
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
 }
 
-edit.addEventListener('click', editProfile);
-
-let closeButton = popup.querySelector('.popup__close-button');
-
+//Закрытие поп-апа
 function closePopup() {
     popup.classList.remove('popup_opened');
 };
 
-closeButton.addEventListener('click', closePopup);
-
-let formElement = document.querySelector('.edit-form');
-let nameInput = formElement.querySelector('.edit-form__name');
-let jobInput = formElement.querySelector('.edit-form__about');
-
+//Заполнения полей формы
 function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    let name = nameInput.value;
-    let about = jobInput.value;
-    let profileName = profile.querySelector('.profile__name');
-    let profileAbout = profile.querySelector('.profile__about');
-    profileName.textContent = name;
-    profileAbout.textContent = about;
-    popup.classList.remove('popup_opened');
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = jobInput.value;
+    closePopup();
 }
-
+//слушатели событий
+edit.addEventListener('click', editProfile);
 formElement.addEventListener('submit', formSubmitHandler); 
+closeButton.addEventListener('click', closePopup);
